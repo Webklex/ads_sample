358d79b052fce85cb9c41de8a0591ab67578b2c1
webklex <info@webklex.com>
2025-11-11 09:49:47 +0100

    Non-nullable property 'Currency' is uninitialized. Adding the 'required' modifier istead of working with a nullable property.

--- ReportService.cs
+++ ReportService.cs
@@ -43,5 +43,5 @@ public class SaleObject
 {
     public DateTime Date { get; set; }
     public double Amount { get; set; }
-    public string Currency { get; set; }
+    public required string Currency { get; set; }
 }
